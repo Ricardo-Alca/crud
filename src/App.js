8a878feee4d9cedba9c0f3aa0a3fd130b51ce6c7
Ricardo-Alca <78196760+Ricardo-Alca@users.noreply.github.com>
2021-02-27 02:38:55 -0600

    Botón de agregar ya funciona

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { isEmpty } from 'lodash'
 import React, { useState } from 'react'
+import shortid from 'shortid'
 
 
 function App() {
 
-  const [task, setTask] = useState(null)
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+
   const addTask = (e) => {
     e.preventDefault()
     if (isEmpty(task)) {
       console.log("Tarea vacia")
       return
     }
-    console.log("OK")
+    
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([ ...tasks, newTask ])
     setTask("")
   }
 
@@ -24,11 +33,15 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la tarea</span>
+          {
+            tasks.map((task) => (
+            <li className="list-group-item" key={task.id}>
+              <span className="lead">{task.name}</span>
               <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
               <button className="btn btn-warning btn-sm float-right">Editar</button>
             </li>
+            ))
+          }
           </ul>
         </div>
         <div className="col-4">
